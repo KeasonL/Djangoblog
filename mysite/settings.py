d67ffa1b484ff4bcb09121c11a4342c8af2139be
KeasonL <ksem4r@126.com>
2020-05-28 12:10:23 +0800

    My Djangoblog app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u2%y)i^cdzxrum8-zqjctrb($^39yhrlob4jyhci27kx*@#aat'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['keasonl.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
